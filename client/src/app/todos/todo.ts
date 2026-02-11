fc1ee944a8702c7321c8adfbb7ea7e295b11f6b2
K.K. Lamberty <lamberty@morris.umn.edu>
2026-02-10 20:03:20 -0600

    Update Todo interface

--- client/src/app/todos/todo.ts
+++ client/src/app/todos/todo.ts
@@ -1,2 +1,9 @@
-export class Todo {
+export interface Todo {
+  _id: string;
+  owner: string;
+  status: boolean;
+  body: string;
+  category: CategoryOption;
 }
+
+export type CategoryOption = 'groceries' | 'homework' | 'video games' | 'software design';
