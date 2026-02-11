5c7f9f76c78f03fc37b628945b2cb7b19ff468d8
K.K. Lamberty <lamberty@morris.umn.edu>
2026-02-10 20:05:30 -0600

    Update TodoService and testing thereof

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Todo } from './todo';
 
+/**
+ * Service that provides the interface for getting information
+ * about `Todos` from the server.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  // The private `HttpClient` is *injected* into the service
+  // by the Angular framework. This allows the system to create
+  // only one `HttpClient` and share that across all services
+  // that need it, and it allows us to inject a mock version
+  // of `HttpClient` in the unit tests so they don't have to
+  // make "real" HTTP calls to a server that might not exist or
+  // might not be currently running.
+  private httpClient = inject(HttpClient);
+
+  // The URL for the todos part of the server API.
+  readonly todoUrl: string = `${environment.apiUrl}todos`;
+
+  getTodos(): Observable<Todo[]> {
+    const httpParams: HttpParams = new HttpParams();
+
+    // Send the HTTP GET request with the given URL and parameters.
+    // That will return the desired `Observable<Todo[]>`.
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
+
+  }
 
 }
